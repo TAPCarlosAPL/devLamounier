567388707db404663bc0a6f1b1ff6befff68f203
TAPCarlosAPL <carlos.lamounier@sempreceub.com>
2023-01-04 02:51:51 -0300

    Versão 0.10

--- cassinolandiaAPP/src/cassinolandiaAPP/App.java
+++ cassinolandiaAPP/src/cassinolandiaAPP/App.java
@@ -8,6 +8,7 @@ public class App {
 		Scanner num = new Scanner(System.in);		
 		
 	//System.out.println("BEM-VINDO AO CASSINOLANDIA\nDigite um numero para escolher a modalidade de jogo");
+	//...
 	//System.out.println("1-SOMALANDIA:\nDESCRIÇÃO: DOIS NUMEROS DE 0 A 10 SERAO GERADOS ALEATORIAMENTE E EM SEGUIDA SERAO SOMADOS, MULTIPLICADOS POR 1000 E DIVIDOS POR 10.\nCOMO JOGAR: DIGITE UM NUMERO DE 0 A 100 PARA SER O SEU RESULTADO ESPERADO\nCASO ACERTE O RESULTADO VOCE GANHARA A PARTIDA.\nBOA SORTE!!");
 	double numDigitado = num.nextDouble();
 	double resultado = OperacaoMatRandomizada.somaRandomizada(0, 0);
