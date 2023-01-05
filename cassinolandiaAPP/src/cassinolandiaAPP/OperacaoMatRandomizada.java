d99da76f2c2e8f6761031733391376a54bfbe77c
TAPCarlosAPL <carlos.lamounier@sempreceub.com>
2023-01-04 22:21:32 -0300

    Versão 0.12

--- cassinolandiaAPP/src/cassinolandiaAPP/OperacaoMatRandomizada.java
+++ cassinolandiaAPP/src/cassinolandiaAPP/OperacaoMatRandomizada.java
@@ -7,17 +7,23 @@ public class OperacaoMatRandomizada {
 	public static int somaRandomizada (){
 		Random numeroAleatorio1 = new Random();
 		int valorRandom1 = numeroAleatorio1.nextInt(50) +1;
+		//System.out.println("TESTE: " + valorRandom1);
 		Random numeroAleatorio2 = new Random();
 		int valorRandom2 = numeroAleatorio2.nextInt(50) +1;
+		//System.out.println("TESTE: " + valorRandom2);
 		int calculoRandomizado = valorRandom1 + valorRandom2;
+		//System.out.println("TESTE: " + calculoRandomizado);
 		return calculoRandomizado;
 	}
 	public static int multiplicacaoRandomizada (){
 		Random numeroAleatorio1 = new Random();
 		int valorRandom1 = numeroAleatorio1.nextInt(10) +1;
+		//System.out.println("TESTE: " + valorRandom1);
 		Random numeroAleatorio2 = new Random();
 		int valorRandom2 = numeroAleatorio2.nextInt(10) +1;
+		//System.out.println("TESTE: " + valorRandom2);
 		int calculoRandomizado = valorRandom1 * valorRandom2;
+		//System.out.println("TESTE: " + calculoRandomizado);
 		return calculoRandomizado;
 	}
 }
